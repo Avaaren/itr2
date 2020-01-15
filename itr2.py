4b4871519d36ef131c7251d05372b28cb60e1f18
Avaaren <avaaren@mail.ru>
2020-01-15 12:33:10 +0300

    checkpoint

--- itr2.py
+++ itr2.py
@@ -1,4 +1,4 @@
-import sys, argparse, random, csv
+import sys, argparse, random, csv,string
 
 from mimesis import Generic
 from mimesis.builtins import RussiaSpecProvider
@@ -6,17 +6,20 @@ from mimesis.enums import Gender
 import time 
 import json
 import requests
+from faker import Faker
 
-
+gender = ['male', 'female']
 start_time = time.time()
-
+faker_by = Faker('be_BY')
+faker_en = Faker('en_US')
+faker_ru = Faker('ru_RU')
 
 #----LOCALES----
 en = 'en'
 ru = 'ru'
+by = 'by'
 FILENAME = 'user_data.csv'
-API = 'trnsl.1.1.20200112T220923Z.5d6803ff56ec93a3.4c200fab657566d38fc110de249beee8b8323f53'
-URL = f'https://translate.yandex.net/api/v1.5/tr.json/translate?lang=ru-be&key={API}'
+
 
 operations = ['delete', 'move', 'add']
 parameters = ['full_name', 'address', 'phone_number']
@@ -34,25 +37,39 @@ def choice_operation_method(string_to_mistake, choice_of_operation,choice_of_cha
     if choice_of_operation == 'delete':
         string_to_mistake = string_to_mistake.replace(string_to_mistake[choice_of_char], '')
     if choice_of_operation == 'move':
-        string_to_mistake = string_to_mistake.replace(string_to_mistake[3], string_to_mistake[5])
+        # print(string_to_mistake)
+        # print(string_to_mistake[choice_of_char])
+        string_to_mistake = string_to_mistake.replace(string_to_mistake[choice_of_char], string_to_mistake[choice_of_char+1])
     if choice_of_operation == 'add':
-        string_to_mistake = string_to_mistake[:choice_of_char]+'f'+string_to_mistake[choice_of_char:]
+        string_to_mistake = string_to_mistake[:choice_of_char]+random.choice(string.ascii_letters)+string_to_mistake[choice_of_char:]
     return string_to_mistake
 
 
 def make_mistakes(choice, string_to_mistake):
     if choice == 'full_name':
-        choice_of_char = random.randint(0, len(string_to_mistake)-1)
-        choice_of_operation = random.choice(operations)
-        string_to_mistake = choice_operation_method(string_to_mistake, choice_of_operation,choice_of_char)
+        if len(string_to_mistake)-2 >= 1:
+            choice_of_char = random.randint(0, len(string_to_mistake)-2)
+        
+            choice_of_operation = random.choice(operations)
+            string_to_mistake = choice_operation_method(string_to_mistake, choice_of_operation,choice_of_char)
+        else:
+            pass
     if choice == 'address':
-        choice_of_char = random.randint(0, len(string_to_mistake)-1)
-        choice_of_operation = random.choice(operations)
-        string_to_mistake = choice_operation_method(string_to_mistake, choice_of_operation,choice_of_char)
+        if len(string_to_mistake)-2 >= 1:
+            choice_of_char = random.randint(0, len(string_to_mistake)-2)
+        
+            choice_of_operation = random.choice(operations)
+            string_to_mistake = choice_operation_method(string_to_mistake, choice_of_operation,choice_of_char)
+        else:
+            pass
     if choice == 'phone_number':
-        choice_of_char = random.randint(0, len(string_to_mistake)-1)
-        choice_of_operation = random.choice(operations)
-        string_to_mistake = choice_operation_method(string_to_mistake, choice_of_operation,choice_of_char)
+        if len(string_to_mistake)-2 >= 1:
+            choice_of_char = random.randint(0, len(string_to_mistake)-2)
+        
+            choice_of_operation = random.choice(operations)
+            string_to_mistake = choice_operation_method(string_to_mistake, choice_of_operation,choice_of_char)
+        else:
+            pass
         
     return string_to_mistake
 
@@ -61,62 +78,55 @@ def errors_below_one(number_of_errors):
     while (number_of_errors < 1):
         number_of_errors = number_of_errors * 10
         k = k*10
-    number_of_strings_for_error = number_of_errors * k
-    return number_of_strings_for_error
+    
+    return number_of_errors*k
 
 def create_list(locale, cl_locale, number_of_strings, number_of_errors):
     generic = Generic(locale)
-    person_gender = random.choice(list(Gender))
-    r = RussiaSpecProvider()
-
+    person_gender = random.choice(gender)
     for i in range(number_of_strings):
-        ###---Full-name----###
         if locale == 'ru':
+            address = faker_ru.street_address()
+            if person_gender == 'male':
+                full_name = f'{faker_ru.last_name_male()} {faker_ru.first_name_male()} {faker_ru.middle_name_male()}'
+            else:
+                full_name = f'{faker_ru.last_name_female()} {faker_ru.first_name_female()} {faker_ru.middle_name_female()}'
+        if locale == 'en':
+            address = faker_en.street_address()
+            if person_gender == 'male':
+                full_name = f'{faker_en.last_name_male()} {faker_en.first_name_male()} {faker_en.middle_name_male()}'
+            else:
+                full_name = f'{faker_en.last_name_female()} {faker_en.first_fename_male()} {faker_en.middle_name_female()}'
+        if locale == 'by':
+            address = faker_by.street_address()
+            if person_gender == 'male':
+                full_name = f'{faker_by.last_name_male()} {faker_by.first_name_male()} {faker_by.middle_name_male()}'
+            else:
+                full_name = f'{faker_by.last_name_female()} {faker_by.first_fename_male()} {faker_by.middle_name_female()}'
             
-            full_name = f'{generic.person.full_name(gender = person_gender)} {r.patronymic(gender = person_gender)}'
-            if cl_locale == 'be_BY':
-                params1 = {'text':full_name}
-                response = requests.get(URL, params=params1)
-                full_name = response.json()['text'][0]
-                # full_name = response.json()[text]
-        else:
-            full_name = f'{generic.person.full_name(gender = person_gender)}'
-        
-        
-            ###########Address###########
-        address = f'{generic.address.address()}' 
-        if locale == 'ru':
-            address = address[:-1] 
-            if cl_locale == 'be_BY':
-                params1 = {'text':address}
-                response = requests.get(URL, params=params1)
-                address = response.json()['text'][0]
-
-###################Phone ###################
         if cl_locale == 'us_US':
             phone_number = generic.person.telephone('-1 (###) ###-####')
         if cl_locale == 'ru_RU':
             phone_number = generic.person.telephone('-7 (###) ###-##-##')
         if cl_locale == 'be_BY':
             phone_number = generic.person.telephone('-8 (###) ###-##-##')  
-       #MAKING ERRORS
-        if number_of_errors < 1:
-            stringsToError = errors_below_one(number_of_errors)
+
+        if number_of_errors < 1 and number_of_errors !=0:
+            stringsToError = int(errors_below_one(number_of_errors))
             if i == 0:
                 pass
             elif (i / stringsToError >= 1 and i % stringsToError == 0):
-                # print (i / stringsToError)
-                # print (stringsToError % i)
-                # print(i)
                 choice = random.choice(parameters)
                 if choice == 'full_name':
                     full_name = make_mistakes(choice, full_name)
                 if choice == 'address':
                     address = make_mistakes(choice, address)
                 if choice == 'phone_number':
-                    phone = make_mistakes(choice, phone_number)
-
+                    phone_number = make_mistakes(choice, phone_number)
+        if number_of_errors == 0:
+            pass
         else:
+            number_of_errors = int(number_of_errors)
             for error_number in range(number_of_errors):
                 choice = random.choice(parameters)
                 if choice == 'full_name':
@@ -124,7 +134,7 @@ def create_list(locale, cl_locale, number_of_strings, number_of_errors):
                 if choice == 'address':
                     address = make_mistakes(choice, address)
                 if choice == 'phone_number':
-                    phone = make_mistakes(choice, phone_number)
+                    phone_number = make_mistakes(choice, phone_number)
             
 
 
@@ -163,7 +173,7 @@ if __name__ == '__main__':
                 writer.writerow(line)
 
     if locale == 'be_BY':
-        list_data = create_list(ru, locale, number_of_strings, number_of_errors)        
+        list_data = create_list(by, locale, number_of_strings, number_of_errors)        
         with open(FILENAME, 'w', newline='', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file, delimiter = ';')
             for line in list_data:
